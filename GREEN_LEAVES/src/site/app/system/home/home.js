2a71553d0db20d60f2a46ecfd73be42a32b2f6ac
Mohan Jayamuni <hjchanna@gmail.com>
2016-09-26 12:08:43 +0530

    green leaves receive, advance request, advance approve, weigh ui finish :+1:

--- GREEN_LEAVES/src/site/app/system/home/home.js
+++ GREEN_LEAVES/src/site/app/system/home/home.js
@@ -1,10 +1,30 @@
 (function () {
-    //module
+//module
     angular.module("homeModule", []);
-
     //controller
     angular.module("homeModule")
-            .controller("homeController", function ($scope) {
-
+            .controller("homeController", function ($scope, $timeout) {
+//                angular.element(document.querySelectorAll(".btn")).on('click', function (event) {
+//                    event.preventDefault();
+//
+//                    var $div = angular.element('<div/>'),
+//                            btnOffset = this.getBoundingClientRect(),
+//                            xPos = event.pageX - btnOffset.left,
+//                            yPos = event.pageY - btnOffset.top;
+//
+//                    $div.addClass('ripple-effect');
+//                    $div.css("height", this.offsetHeight + "px");
+//                    $div.css("width", this.offsetHeight + "px");
+//                    $div.css({
+//                        top: yPos - (this.offsetHeight / 2) + "px",
+//                        left: xPos - (this.offsetHeight / 2) + "px"
+//                    });
+//                    angular.element(this).append($div);
+//
+//                    $timeout(function () {
+//                        $div.remove();
+//                        console.log("removed");
+//                    }, 1800);
+//                });
             });
 }());
