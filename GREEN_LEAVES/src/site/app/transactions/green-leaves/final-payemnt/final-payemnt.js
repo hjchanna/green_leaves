05b36f3ce1f3edf4a6463e0e21e0fe2bdc14cf53
kavish perera <kavishmanjitha@gmail.com>
2016-09-27 17:12:03 +0530

    final payment price settings

--- GREEN_LEAVES/src/site/app/transactions/green-leaves/final-payemnt/final-payemnt.js
+++ GREEN_LEAVES/src/site/app/transactions/green-leaves/final-payemnt/final-payemnt.js
@@ -5,5 +5,26 @@
     //controller
     angular.module("finalPaymentModule")
             .controller("finalPaymentModuleController", function ($scope) {
+                $scope.otherValues = true;
+                //A /Payment,L/Installment,Fertilizer(P),Fertilizer(C),Tea,Savings,Other
+                $scope.showOther = function () {
+                    $scope.otherValues = true;
+                    $scope.showGreenLeaveValues = false;
+                    $scope.showBfAndCoinValues = false;
+                };
+                 
+                //GL Value
+                $scope.showGLValues = function () {
+                    $scope.showGreenLeaveValues = true;
+                    $scope.otherValues = false;
+                    $scope.showBfAndCoinValues = false;
+                };
+                
+                //B/F,Coins
+                $scope.showBfAndCoins = function () {
+                    $scope.showBfAndCoinValues = true;
+                    $scope.otherValues = false;
+                    $scope.showGreenLeaveValues = false;
+                };
             });
 }());
