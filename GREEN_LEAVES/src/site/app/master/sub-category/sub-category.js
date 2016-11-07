b436938ed4363e8ddb847f80ea3f61fdae591b13
kalum323 <kalum323@gmail.com>
2016-11-07 11:00:31 +0530

    sub category changes

--- GREEN_LEAVES/src/site/app/master/sub-category/sub-category.js
+++ GREEN_LEAVES/src/site/app/master/sub-category/sub-category.js
@@ -3,7 +3,7 @@
 
     //http factory
     angular.module("subCategoryModule")
-            .factory("subCategoryModule", function ($http, systemConfig) {
+            .factory("subCategoryFactory", function ($http, systemConfig) {
                 var factory = {};
                 //load
                 factory.loadCategory = function (callback) {
@@ -17,11 +17,23 @@
 
                             });
                 };
+                factory.loadSubCategory = function (callback) {
+                    var url = systemConfig.apiUrl + "/api/green-leaves/master/subCategory";
 
+                    $http.get(url)
+                            .success(function (data, status, headers) {
+                                callback(data);
+                            })
+                            .error(function (data, status, headers) {
+
+                            });
+                };
+                return factory
             });
 
+
     angular.module("subCategoryModule")
-            .controller("subCategoryController", function ($scope, categoryFactory, Notification) {
+            .controller("subCategoryController", function ($scope, subCategoryFactory, Notification) {
                 //data models 
                 $scope.model = {};
 
@@ -31,15 +43,32 @@
                 //http models
                 $scope.http = {};
 
-                $scope.model.category = [];
+                $scope.model.categoryList = [
+                    {indexNo: 1, name: "Category 01"},
+                    {indexNo: 2, name: "Category 02"},
+                    {indexNo: 3, name: "Category 03"},
+                    {indexNo: 4, name: "Category 04"}
+                ];
+                
+                $scope.model.subCategoryList=[
+                    {indexNo: 1, name:"Sub Category name", category:"Category 01"},
+                    {indexNo: 2, name:"Sub Category name", category:"Category 02"},
+                    {indexNo: 3, name:"Sub Category name", category:"Category 03"}
+                ];
 
                 $scope.ui.init = function () {
+
+                    $scope.ui.mode = "IDEAL";
                     //load category
-                    categoryFactory.loadCategory(function (data) {
+                    subCategoryFactory.loadCategory(function (data) {
                         console.log(data);
                     });
-                };
+                    subCategoryFactory.loadSubCategory(function (data) {
+                        console.log(data);
+                    });
 
+
+                };
                 $scope.ui.init();
 
             });
