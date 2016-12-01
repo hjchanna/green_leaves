b46094284376b0b63b95d27a5df91dce05540598
Mohan Jayamuni <hjchanna@gmail.com>
2016-12-01 13:14:25 +0530

    home.js commit

--- GREEN_LEAVES/src/site/app/system/home/home.js
+++ GREEN_LEAVES/src/site/app/system/home/home.js
@@ -3,29 +3,39 @@
     angular.module("homeModule", []);
     //controller
     angular.module("homeModule")
-            .controller("homeController", function ($scope, $timeout, optionPane) {
+            .controller("homeController", function ($scope, $timeout, optionPane, ConfirmPane, ProgressPane) {
                 $scope.informationMessage = function () {
 //                    optionPane.open('danger');
                     optionPane.defaultMessage("My message should be here");
                 };
-                
-                $scope.default = function(){
+
+                $scope.default = function () {
                     optionPane.defaultMessage("My message should be here");
                 };
-                $scope.primary = function(){
-                    optionPane.primaryMessage("My message should be here");
+                $scope.primary = function () {
+//                    optionPane.primaryMessage("My message should be here");
+                    ConfirmPane.primaryConfirm("Hello World")
+                            .confirm(function () {
+                                console.log("DONE");
+                            })
+                            .discard(function () {
+                                console.log("ReJECT");
+                            });
                 };
-                $scope.info = function(){
+                $scope.info = function () {
                     optionPane.infoMessage("My message should be here");
                 };
-                $scope.success = function(){
-                    optionPane.successMessage("My message should be here");
+                $scope.success = function () {
+//                    optionPane.successMessage("My message should be here");
+                    ProgressPane.successProgress("Loading...").close();
                 };
-                $scope.warning = function(){
+                $scope.warning = function () {
                     optionPane.warningMessage("My message should be here");
                 };
-                $scope.danger = function(){
+                $scope.danger = function () {
                     optionPane.dangerMessage("My message should be here");
                 };
+
+
             });
 }());
